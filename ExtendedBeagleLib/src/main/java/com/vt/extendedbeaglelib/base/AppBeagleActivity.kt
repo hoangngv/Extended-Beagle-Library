aa21506a07aab93e24cfe347b35a0d9fc665737c
hoangnv65 <hoangnv65@viettel.com.vn>
2021-05-11 14:57:54 +0700

    update Beagle activity

--- ExtendedBeagleLib/src/main/java/com/vt/extendedbeaglelib/base/AppBeagleActivity.kt
+++ ExtendedBeagleLib/src/main/java/com/vt/extendedbeaglelib/base/AppBeagleActivity.kt
@@ -1,64 +1,21 @@
 package com.vt.extendedbeaglelib.base
 
-import android.content.Context
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
 import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.FileProvider
 import br.com.zup.beagle.android.annotation.RegisterController
 import br.com.zup.beagle.android.view.BeagleActivity
 import br.com.zup.beagle.android.view.ServerDrivenState
 import com.vt.extendedbeaglelib.R
-import com.vt.extendedbeaglelib.common.CapturePhotoListener
-import com.vt.extendedbeaglelib.common.Constants.PERMISSION_CAMERA
-import com.vt.extendedbeaglelib.common.Constants.PERMISSION_PHOTO
-import com.vt.extendedbeaglelib.common.PickLocalPhotoListener
-import com.vt.extendedbeaglelib.utils.bus.SingleBus
-import com.vt.extendedbeaglelib.utils.bus.SingleBusKey
-import com.vt.extendedbeaglelib.utils.extensions.toast
-import com.wang.avi.AVLoadingIndicatorView
 import kotlinx.android.synthetic.main.activity_beagle.*
-import kotlinx.coroutines.Job
-import java.io.File
 
-@RegisterController
+@RegisterController(id = "ACTIVITY_IDENTIFIER")
 class AppBeagleActivity : BeagleActivity() {
-
-    private lateinit var uri: Uri
-
-    private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicture()) { isSaved ->
-        if (isSaved) {
-            Log.d("dLog", uri.toString())
-            captureListener?.onCaptureSuccess(uri)
-        } else {
-            Log.d("dLog", "Image isn't saved")
-            captureListener?.onCaptureFailure()
-        }
-    }
-
-    private val pickLocalImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-        if (uri != null) {
-            pickPhotoListener?.onPickPhotoSuccess(uri)
-        } else {
-            pickPhotoListener?.onPickPhotoFailure()
-        }
-    }
-
-    companion object {
-        var captureListener: CapturePhotoListener? = null
-        var pickPhotoListener: PickLocalPhotoListener? = null
-        val busJobs : MutableList<Job> = arrayListOf()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beagle)
-        initBus(this)
     }
 
     override fun getServerDrivenContainerId(): Int = R.id.server_driven_container
@@ -66,79 +23,15 @@ class AppBeagleActivity : BeagleActivity() {
     override fun getToolbar(): Toolbar = findViewById(R.id.toolbar)
 
     override fun onServerDrivenContainerStateChanged(state: ServerDrivenState) {
-        if (state is ServerDrivenState.Error) {
-            Log.d("dLog", "Error")
-        } else if (state is ServerDrivenState.Loading) {
-            (progress_bar as AVLoadingIndicatorView).visibility = if (state.loading) View.VISIBLE else View.GONE
-        }
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        finish()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSION_CAMERA) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                toast(resources.getString(R.string.camera_granted))
-                capturePhoto(this)
-            } else {
-                toast(resources.getString(R.string.camera_denied))
+        when (state) {
+            is ServerDrivenState.Started -> {
+                progress_bar.visibility = View.VISIBLE
             }
-        } else if (requestCode == PERMISSION_PHOTO) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                toast(resources.getString(R.string.storage_granted))
-                pickImage()
-            } else {
-                toast(resources.getString(R.string.storage_denied))
+
+            is ServerDrivenState.Finished -> {
+                progress_bar.visibility = View.GONE
             }
-        }
-    }
-
-    private fun initBus(context: Context) {
-        busJobs.apply {
-            add(SingleBus.receive(SingleBusKey.CAPTURE_PHOTO) {
-                capturePhoto(context)
-            })
-
-            add(SingleBus.receive(SingleBusKey.PICK_LOCAL_IMAGE) {
-                pickImage()
-            })
-        }
-    }
-
-    private fun capturePhoto(context: Context) {
-        val photoFile = File.createTempFile(
-            "IMG_",
-            ".jpg",
-            context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        )
-
-        uri = FileProvider.getUriForFile(
-            context,
-            "${context.packageName}.provider",
-            photoFile
-        )
-
-        takePicture.launch(uri)
-    }
-
-    private fun pickImage() {
-        // pass in the mime type
-        pickLocalImage.launch("image/*")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("dLog", "onDestroy --- busJobs = ${busJobs.size}")
-        busJobs.forEach {
-            it.cancel()
+            else -> Log.d("dLog", "State -> $state")
         }
     }
 }
